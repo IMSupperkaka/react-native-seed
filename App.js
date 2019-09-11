0603ab25034c24fb04726e841c86ef3075b71dcc
Shiyun Hua <1072815089@qq.com>
2019-09-11 15:33:26 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: Home,
+    Detail: Detail
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
